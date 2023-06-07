fcd23326576a660ae6174a452fdddcd5b9754340
yakisova41 <yakisoba5emon@gmail.com>
2023-06-07 12:52:48 +0000

    ✨ サムネイルの正方形化

--- src/niconico/NicoPlayer.tsx
+++ src/niconico/NicoPlayer.tsx
@@ -14,16 +14,20 @@ const NicoPlayer: React.FunctionComponent = () => {
         initialWatchData?.data.owner?.nickname ??
         initialWatchData?.data.channel.name;
 
-      navigator.mediaSession.metadata = new MediaMetadata({
-        title: initialWatchData?.data.video.title,
-        artist: nickname,
-        artwork: [
-          {
-            src: String(initialWatchData?.data.video.thumbnail.player),
-            sizes: "960x540",
-            type: "image/jpeg",
-          },
-        ],
+      void trimArtWork(
+        String(initialWatchData?.data.video.thumbnail.player)
+      ).then((artworkURL) => {
+        navigator.mediaSession.metadata = new MediaMetadata({
+          title: initialWatchData?.data.video.title,
+          artist: nickname,
+          artwork: [
+            {
+              src: artworkURL,
+              sizes: "540x540",
+              type: "image/png",
+            },
+          ],
+        });
       });
     }
   }, [initialWatchData]);
@@ -45,5 +49,29 @@ const NicoPlayer: React.FunctionComponent = () => {
     </div>
   );
 };
+
+/**
+ * サムネイルの正方形化
+ */
+async function trimArtWork(artWorkSrc: string): Promise<string> {
+  return await new Promise((resolve) => {
+    const canvas = document.createElement("canvas");
+    const size = 540;
+    const originalWidth = 960;
+    canvas.width = size;
+    canvas.height = size;
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+    img.onload = () => {
+      ctx?.drawImage(img, ((originalWidth - size) / 2) * -1, 0);
+      const dataURL = canvas.toDataURL("image/png");
+      canvas.remove();
+      resolve(dataURL);
+    };
+    img.crossOrigin = "anonymous";
+    img.src = artWorkSrc;
+  });
+}
 
 export default NicoPlayer;
